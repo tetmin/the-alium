a17736369b4bba83ebc0503e78d1e9b34f228afa
Tom Etminan <tetmin@gmail.com>
2023-05-29 21:58:57 +0100

    jekyll fixes & server.py local+remote modes

--- server.py
+++ server.py
@@ -4,9 +4,10 @@ import openai
 import os
 import re
 
-# Setup the Modal Labs stub
+# Setup the Modal Labs image
 image = modal.Image.debian_slim().poetry_install_from_file("pyproject.toml")
 stub = modal.Stub(name="dAIly-mash", image=image)
+
 
 # Get some topical news article headlines
 def get_news_articles(api_key, query, n_articles):
@@ -27,6 +28,7 @@ def get_news_articles(api_key, query, n_articles):
 
     return titles
 
+
 # OpenAI completion boilerplate
 def get_completion(prompt, model="gpt-3.5-turbo"):
     messages = [{"role": "user", "content": prompt}]
@@ -35,7 +37,8 @@ def get_completion(prompt, model="gpt-3.5-turbo"):
     )
     return response.choices[0].message["content"]
 
-# Clean up the filename for Jekyll
+
+# Clean up a filename for Jekyll
 def clean_filename(filename):
     # Remove illegal characters
     cleaned = re.sub(r'[\\/:"*?<>|]', "", filename)
@@ -49,34 +52,46 @@ def clean_filename(filename):
     return cleaned
 
 
-# TODO: Update to add articles to the _posts directory from remote server
-def write_markdown_file(file_path, content):
-    with open(file_path, "w") as f:
-        f.write(content)
+# Generate an article from a news article title
+def generate_article_from_title(title):
+    prompt = f"""Write a parody of this news headline: ' {title} ' in the style of The Daily Mash, with any proper names changed to humourous ones. 
+    Make the article no more than 200 words long. Include Markdown formatting for jekyll."""
 
-# Generate a Jekyll post from a news article title
-def generate_post_from_title(title, prompt):
     content = get_completion(prompt)
-    filename = clean_filename(title)
-    file_path = f"{filename}.md"
-    write_markdown_file(file_path, content)
+    return content
 
 
-@stub.function(secrets=[modal.Secret.from_name("mark-openai-secret"),modal.Secret.from_name("mark-gnews-secret")])
-def generate_posts(query, prompt, n_articles):
+@stub.function(
+    secrets=[
+        modal.Secret.from_name("mark-openai-secret"),
+        modal.Secret.from_name("mark-gnews-secret"),
+    ]
+)
+def generate_posts(query, n_articles):
     titles = get_news_articles(os.environ["GNEWS_API_KEY"], query, n_articles)
 
-    for title in titles:
-        generate_post_from_title(title, prompt)
+    articles = [generate_article_from_title(title) for title in titles]
+    filenames = [clean_filename(title) for title in titles]
+
+    posts = zip(filenames, articles)
+
+    return posts
 
 
 # Specify the query filter for articles (e.g., "artificial intelligence")
 query = "artificial intelligence"
 
-# Specify the prompt for generating the satirical article
-prompt = f"Write a parody of this news headline in the style of The Daily Mash, with any proper names changed to humourous ones. Make the article no more than 200 words long. Include Markdown formatting for jekyll."
-
 
 @stub.local_entrypoint()
 def main():
-    generate_posts.call(query, prompt, 1)
+    posts = generate_posts.call(query, 1)
+
+    # write each post to a file
+    for filename, content in posts:
+        with open(f"_posts/{filename}.md", "w") as f:
+            f.write(content)
+
+
+@stub.function(schedule=modal.Period(hours=5))
+def scheduled():
+    posts = generate_posts.call(query, 1)
